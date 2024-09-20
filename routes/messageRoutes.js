6f0bae5a3212ffa6ec7b977f6bf9467daf43b0a9
nikburner <rajnikhil1175@gmail.com>
2024-09-20 18:18:54 +0530

    completed the whole project, need to finished the group chat feature

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,14 +3,7 @@ const router = express.Router();
 const { sendMessage, getMessages } = require('../controllers/messageController');
 const { authenticateToken } = require('../utils/authMiddleware');
 
-<<<<<<< HEAD
 router.post('/', authenticateToken, sendMessage);
 router.get('/:otherUserId', authenticateToken, getMessages);
 
 module.exports = router;
-=======
-router.post('/:groupId', authenticateToken, sendMessage);
-router.get('/:groupId', authenticateToken, getMessages);
-
-module.exports = router;
->>>>>>> 268b1808510ad8ec53727b2f3504d27760b2a060
